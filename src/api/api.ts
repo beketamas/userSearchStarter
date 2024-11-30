e8a8b6af9e0c0b94430fa7baa384dafa183dd1c8
LLorintz <lorintz.levente@mechwart.com>
2024-11-30 10:45:57 +0100

    Delete src/api directory

--- src/api/api.ts
+++
@@ -1,7 +0,0 @@
-export const fetchUsers = async () => {
-    const response = await fetch("/data.json");
-    const data = await response.json();
-    console.log(data.users); // JSON adat
-    return data.users
-  };
-
